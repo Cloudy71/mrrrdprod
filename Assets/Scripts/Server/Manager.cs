2c7a249e24a485653c03c16059976042f87804c5
Rades98 <reznicek98@seznam.cz>
2018-11-02 19:11:53 +0100

    added: - CombatSystem - PlayerCombat - Naplnění zbraní - WeaponsFiller

--- Assets/Scripts/Server/Manager.cs
+++ Assets/Scripts/Server/Manager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class Manager : NetworkManager {
 
+    Hackathon.PlayerSelection ps = new Hackathon.PlayerSelection();
+    Hackathon.WeaponsFiller wp = new Hackathon.WeaponsFiller();
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -25,5 +27,9 @@ public class Manager : NetworkManager {
         }
 
         // db věci..
+        Debug.Log("server started..");
+       
+        wp.Create();
+        ps.CmdCharacterSelection("hovno", 1);
     }
 }
--- Assets/Scripts/Weapons/WeaponsFiller.cs
+++ Assets/Scripts/Weapons/WeaponsFiller.cs
@@ -7,16 +7,25 @@ namespace Hackathon
     class WeaponsFiller
     {
         public void Create()
+        {                 // NAME      range    DMG    cost    ammo    accuracy
+            CreateWeapon("LMG",         4,      70,     8,      200,    70);
+            CreateWeapon("Sniperka",    6,      100,    5,      5,      70);
+            CreateWeapon("CZ vz. 75",   3,      70,     2,      7,      90);
+            CreateWeapon("Brokovnice",  1,      120,    5,      7,      90);
+            CreateWeapon("AK-74U",      4,      90,     4,      35,     100);     
+        }
+
+        public void CreateWeapon(string name, int range, int damage, int cost, int ammo, int accuracy)
         {
             Weapon w = new Weapon();
-            /*  w.ID = 
-              w.Name 
-              w.Range
-              w.Accuracy
-              w.Ammo
-              w.Cost
-              w.Damage
-              WeaponTable.Insert(w);*/
+            w.ID = WeaponTable.Select_Count() + 1;
+            w.Name = name;
+            w.Range = 1 + range;
+            w.Damage = damage;
+            w.Cost = cost;
+            w.Ammo = ammo;
+            w.Accuracy = accuracy;
+            WeaponTable.Insert(w);
         }
     }
 }
