2a74099aa2d5102113afc64b11163133a687fc90
Cloudy <crosska111@gmail.com>
2018-11-02 23:02:23 +0100

    PlayerData

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerData : NetworkBehaviour {
+    [SyncVar]
+    public string Name;
 
-    [SyncVar] public string Name;
-    [SyncVar] public int Armor;
-    [SyncVar] public int Health;
-    [SyncVar] public int Score;
-    [SyncVar] public int Inventory_ID;
-    [SyncVar] public int Character_ID;
+    [SyncVar]
+    public int Armor;
+
+    [SyncVar]
+    public int Health;
+
+    [SyncVar]
+    public int Score;
+
+    [SyncVar]
+    public int Inventory_ID;
+
+    [SyncVar]
+    public int Character_ID;
+
+    [SyncVar]
+    public Vector2 GridPosition;
+
+    [SyncVar]
+    public Vector2 MovePosition;
 
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
 }
