9b4adf4662fc3348e637d9dd41afa5d38d8db993
Cloudy <crosska111@gmail.com>
2018-11-02 19:31:02 +0100

    outline + map gen

---
+++ Assets/Scripts/Game/Map.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    [HideInInspector]
+    public List<GameObject> Blocks;
+
+    public Vector2 Size;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public GameObject GetBlockOnPosition(Vector2 position) {
+        int idx = (int) (position.x * Size.y + position.y);
+        return Blocks[idx];
+    }
+
+    public GameObject GetBlockOnPosition(int x, int y) {
+        return GetBlockOnPosition(new Vector2(x, y));
+    }
+}
--- Assets/Scripts/Game/MapGenerator.cs
+++ Assets/Scripts/Game/MapGenerator.cs
@@ -1,16 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEditor;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
+    public GameObject HexagonPrefab;
+    public int        Width;
+    public int        Height;
+    public Material   GroundMaterial;
+    public Material   WallMaterial;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Map _map;
+
+    public static readonly MapOffset[] MapOffsets = new MapOffset[] {
+        new MapOffset(1f, 0f),        // Right
+        new MapOffset(-1f, 0f),       // Left
+        new MapOffset(-0.5f, 0.75f),  // TopLeft
+        new MapOffset(0.5f, 0.75f),   // TopRight
+        new MapOffset(-0.5f, -0.75f), // BottomLeft
+        new MapOffset(0.5f, -0.75f),  // BottomRight
+        new MapOffset(0f, 0f),        // Empty 
+    };
+
+    // Use this for initialization
+    void Start() {
+        _map = GetComponent<Map>();
+        _map.Size = new Vector2(Width, Height);
+        _map.Blocks = new List<GameObject>();
+
+        for (int i = 0; i < Width; i++) {
+            bool left = false;
+            for (int j = 0; j < Height; j++) {
+                Vector3 pos = new Vector3(i * MapOffsets[0].x + (left ? MapOffsets[4].x : 0f),
+                                          0f,
+                                          j * MapOffsets[4].y);
+                GameObject gObject = Instantiate(HexagonPrefab, pos, HexagonPrefab.transform.rotation);
+                if (i == 0 || i == Width - 1 || j == 0 || j == Height - 1) {
+                    gObject.GetComponent<MeshRenderer>().material = WallMaterial;
+                }
+                else {
+                    gObject.GetComponent<MeshRenderer>().material = GroundMaterial;
+                }
+
+                left = !left;
+                _map.Blocks.Add(gObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public class MapOffset {
+        public readonly float x;
+        public readonly float y;
+
+        public MapOffset(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
