0c55ccf6dca1aac508bc930e588e2e89cb5fee54
Restrify <rouman95@seznam.cz>
2018-11-02 18:09:33 +0100

    constants

---
+++ Assets/Scripts/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Hackathon
+{
+
+    public class Constants
+    {
+        public readonly int player_basehealth = 100; 
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Hackathon
+{
+    public class PlayerSelection : NetworkBehaviour {
+
+        Constants constants = new Constants();
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        [Command]
+        public void CmdCharacterSelection(string name, int ID)
+        {
+            Player player = new Player();
+            Character c = CharacterTable.Select(ID);
+            Inventory i = new Inventory();
+            Weapon w = WeaponTable.Select(c.Weapon);
+            player.ID = PlayerTable.Select_Count()+1;
+            player.Name = name;
+            player.Health = constants.player_basehealth + c.BHealth;
+            player.Score = 0;
+            player.Armor = 0;
+            player.CharacterID = ID;
+            player.InventoryID = InventoryTable.Select_Count() + 1;
+            PlayerTable.Insert(player);
+
+            
+            i.Current = w.Ammo;
+            i.Player_ID = player.ID;
+            i.Weapon_ID = c.Weapon;
+            i.Slot = InventoryTable.Select_Count() + 1;
+            InventoryTable.Insert(i);
+
+
+
+
+        }
+    }
+}
