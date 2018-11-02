fcf2e5551c4b11f44dd1e2a9ad474b469a97c47f
Rades98 <reznicek98@seznam.cz>
2018-11-02 17:22:16 +0100

    sasfsaf

--- Assets/Scripts/Weapons/WeaponsFiller.cs
+++ Assets/Scripts/Weapons/WeaponsFiller.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Weapons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Hackathon
+{
+    class WeaponsFiller
+    {
+        public void Create()
+        {
+            Weapon w = new Weapon();
+            /*  w.ID = 
+              w.Name 
+              w.Range
+              w.Accuracy
+              w.Ammo
+              w.Cost
+              w.Damage
+              WeaponTable.Insert(w);*/
+        }
+    }
 }
