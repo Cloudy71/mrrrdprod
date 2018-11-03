4744528a050318db2229884f6535dfbbe72129a8
Rades98 <reznicek98@seznam.cz>
2018-11-03 02:46:51 +0100

    bullet data

---
+++ Assets/Scripts/Bullet/BulletData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BulletData : NetworkBehaviour {
+
+
+    [SyncVar]
+    public int Speed;
+
+    [SyncVar]
+    public int AttackerId;
+
+    [SyncVar]
+    public int DefenderId;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+   
+}
