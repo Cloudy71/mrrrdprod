e264f27029f11bc81d656fb26cb1a30f0efdce36
Rades98 <reznicek98@seznam.cz>
2018-11-03 02:28:55 +0100

    Bullet

---
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+    private PlayerData _playerData;
+
+    void Start () {
+        
+        _playerData = GetComponent<PlayerData>();
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
