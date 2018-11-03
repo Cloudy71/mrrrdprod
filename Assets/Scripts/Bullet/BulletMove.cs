5b3bba7401495e1d0512b722f91b1dc94f475613
Restrify <rouman95@seznam.cz>
2018-11-03 02:48:01 +0100

    bulletmove

--- Assets/Scripts/Bullet/BulletMove.cs
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletMove : MonoBehaviour {
+namespace Hackathon
+{
 
-    private PlayerData _playerData;
-
-    void Start () {
-        
-        _playerData = GetComponent<PlayerData>();
-    }
-	
-	void Update ()
+    public class BulletMove : MonoBehaviour
     {
-	
-	}
+
+        private PlayerData _playerData;
+        private GameObject[] players;
+
+        void Start()
+        {
+
+            players = GameObject.FindGameObjectsWithTag("Player");
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && !_playerData.IsMoving)
+            {
+                _playerData.MovePosition = Manager.MANAGER.GetComponent<Map>()
+                                                  .GetGridPositionByBlock(Manager
+                                                                          .MANAGER.GetComponent<MapBlockBehaviour>()
+                                                                          .SelectedBlock);
+                _playerData.IsMoving = true;
+            }
+
+        }
+    }
 }
